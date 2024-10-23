2cdc3ee292c0576564de64aaa406806df24eb5da
Lamrini Imane <125766708+ImaneLamrini@users.noreply.github.com>
2024-10-23 16:51:59 +0100

    Update CommandeRepository.java

--- src/main/java/ma/edeliver/edeliverbackend/repository/CommandeRepository.java
+++ src/main/java/ma/edeliver/edeliverbackend/repository/CommandeRepository.java
@@ -4,7 +4,10 @@ import ma.edeliver.edeliverbackend.entity.Commande;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommandeRepository extends JpaRepository<Commande, Long> {
   // You can add custom query methods if needed.
+  List<Commande> findByLivreurId(Long livreurId);
 }
