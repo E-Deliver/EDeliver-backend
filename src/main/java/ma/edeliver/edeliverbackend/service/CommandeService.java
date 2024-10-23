38cbc49fdcb93a526a64d41b85f508cf3733e50e
Lamrini Imane <125766708+ImaneLamrini@users.noreply.github.com>
2024-10-23 16:49:06 +0100

    Adding Assignation et consultationCmd par Livreur API

--- src/main/java/ma/edeliver/edeliverbackend/service/CommandeService.java
+++ src/main/java/ma/edeliver/edeliverbackend/service/CommandeService.java
@@ -2,8 +2,11 @@ package ma.edeliver.edeliverbackend.service;
 
 import ma.edeliver.edeliverbackend.entity.Commande;
 import ma.edeliver.edeliverbackend.repository.CommandeRepository;
+import ma.edeliver.edeliverbackend.repository.LivreurRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ma.edeliver.edeliverbackend.entity.Livreur;
+
 
 import java.util.List;
 
@@ -23,5 +26,28 @@ public class CommandeService {
     return commandeRepository.findById(id).orElse(null);
   }
 
-  // You can add other CRUD operations if needed
+  // Affecter une commande à un livreur
+  @Autowired
+  private LivreurRepository livreurRepository;
+
+  // Méthode pour assigner une commande à un livreur
+  public Commande assignerCommandeALivreur(Long commandeId, Long livreurId) {
+    Commande commande = commandeRepository.findById(commandeId)
+            .orElseThrow(() -> new IllegalArgumentException("Commande introuvable"));
+
+    Livreur livreur = livreurRepository.findById(livreurId)
+            .orElseThrow(() -> new IllegalArgumentException("Livreur introuvable"));
+
+    commande.setLivreur(livreur);  // Assigner le livreur à la commande
+    return commandeRepository.save(commande);  // Sauvegarder la commande avec le livreur assigné
+  }
+
+//Consulter les commandes affectées par le livreur
+  public List<Commande> getCommandesByLivreurId(Long livreurId) {
+    return commandeRepository.findByLivreurId(livreurId);
+
+  }
+
+
+
 }
