7464a94a79b4c4409dd83c2a314e69348808f69a
Lamrini Imane <125766708+ImaneLamrini@users.noreply.github.com>
2024-10-28 00:29:57 +0100

    Update Parameters.java

--- src/test/java/Parameters.java
+++ src/test/java/Parameters.java
@@ -3,5 +3,11 @@ public class Parameters {
     public static String UrlDashLivreurPage="http://localhost:4200/dashboard-livreur";
     public static String UrlDashClientPage="http://localhost:4200/dashboard-client";
     public static String UrlDashAdminPage="http://localhost:4200/dashboard-administrateur";
+    //***********Historique par Admin*************
+    public static String UrlAdminHistorique="http://localhost:4200/historique";
+    //**********Cmd pour Livreur******************
+    public static String UrlLivreurCmd="http://localhost:4200/commandes-livreur";
+    //**********Historique pour Livreur******************
+    public static String UrlLivreurHistorique="http://localhost:4200/historique-livreur";
     //*****************
 }
