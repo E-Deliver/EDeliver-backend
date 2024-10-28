c188ef822f612d6ef6f341d6cc508a83f42f97d8
Lamrini Imane <125766708+ImaneLamrini@users.noreply.github.com>
2024-10-28 19:09:06 +0100

    Update LivreurProcess.java

--- src/test/java/LivreurProcess.java
+++ src/test/java/LivreurProcess.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -14,9 +16,33 @@ public class LivreurProcess extends Loginlivreur{
             System.out.println("Test Passed: les commandes sont consultables par le client correctement!");
         }
         //Accept some of assigned commands
-        //****************************************************
-        // ********************************************************
-        //********************************************************
+        if(ProjectFunctions.VerifyElements(driver,Project_Xpath.Xpath_confirmerLivraison)==true){
+            //WebElement Confirmer=driver.findElement(Project_Xpath.Xpath_confirmerLivraison);
+            //Confirmer.click();
+            WebElement confirmer = wait.until(ExpectedConditions.visibilityOfElementLocated(Project_Xpath.Xpath_confirmerLivraison));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", confirmer);
+            softAssert.assertTrue(ProjectFunctions.VerifyElements(driver,Project_Xpath.xpathPopUpConfirmLivraison),"Le pop up de demander au livreur s'il est sûr de confirmer la livraison ne s'affiche pas!");
+            if(ProjectFunctions.VerifyElements(driver,Project_Xpath.xpathPopUpConfirmLivraison)){
+                System.out.println("Le pop up de demander au livreur s'il est sûr de confirmer la livraison s'affiche correctement");
+            }
+            //Annuler la livraison
+            WebElement annuler=wait.until(ExpectedConditions.visibilityOfElementLocated(Project_Xpath.xpathAnnulerLivraison));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", annuler);
+            System.out.println("La livraison est annulée avec succée par le livreur");
+            //Accepter la livraison
+            WebElement confirmerNouveau = wait.until(ExpectedConditions.visibilityOfElementLocated(Project_Xpath.Xpath_confirmerLivraison));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", confirmer);
+            Thread.sleep(2000);
+            WebElement acceptLivraison=wait.until(ExpectedConditions.visibilityOfElementLocated(Project_Xpath.Xpath_AccepterLivraison));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", acceptLivraison);
+            softAssert.assertTrue(ProjectFunctions.VerifyElements(driver,Project_Xpath.xpathsuccessacceptLivraison),"Un probléme persiste lors de l'acceptation de livraison par ce livreur!");
+            if(ProjectFunctions.VerifyElements(driver,Project_Xpath.xpathsuccessacceptLivraison)){
+                System.out.println("La livraison est accéptée par le livreur correctement!");
+            }
+            Thread.sleep(1000);
+            WebElement OKLiv= driver.findElement(Project_Xpath.xpathokafteracceptlivraison);
+            OKLiv.click();
+        }
         //
         softAssert.assertTrue(ProjectFunctions.VerifyElements(driver,Project_Xpath.xpathListecmdLivreur),"Un probléme persiste lors d'affichage des commandes assignés à ce livreur!");
         if(ProjectFunctions.VerifyElements(driver,Project_Xpath.xpathListecmdLivreur)){
@@ -35,11 +61,31 @@ public class LivreurProcess extends Loginlivreur{
     if(ProjectFunctions.VerifyElements(driver,Project_Xpath.xpathListeHistoriqueLivreur)){
         System.out.println("Test Passed: La liste des commandes livrés est affichée correctement comme historique");
     }
+    Thread.sleep(1000);
 }
 
-@Test
+/*@Test(priority=3)
+public void CarteConsultation(){
+    WebElement Cart= driver.findElement(Project_Xpath.xpathCartLivreur);
+    Cart.click();
+    softAssert.assertTrue(ProjectFunctions.VerifyUrl(driver,Parameters.UrlLivreurCart));
+    if(ProjectFunctions.VerifyUrl(driver,Parameters.UrlLivreurCart)){
+        System.out.println("Test Passed: la carte de localisation des commandes de ce livreur est correctement consultable");
+    }
+}*/
+/*@Test(priority=3)
     public void EditProfileLivreur(){
 
+}*/
+
+@Test(priority=4)
+    public void SeDeconnecter(){
+    WebElement Logout=driver.findElement(Project_Xpath.logoutlivreur);
+    Logout.click();
+    softAssert.assertTrue(ProjectFunctions.VerifyUrl(driver,Parameters.UrlHomePage),"Ce livreur n'a pas pu se déconnecter");
+    if(ProjectFunctions.VerifyUrl(driver,Parameters.UrlHomePage)){
+        System.out.println("Test Passed: le livreur se déconnecte correctement");
+    }
 }
 
 
