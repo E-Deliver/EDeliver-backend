81c15aae4bc4bd602445bf90a6dd7740cadbf90e
Lamrini Imane <125766708+ImaneLamrini@users.noreply.github.com>
2024-11-03 12:40:24 +0100

    Update Parameters.java

--- src/test/java/Parameters.java
+++ src/test/java/Parameters.java
@@ -9,10 +9,15 @@ public class Parameters {
     public static String UrlLivreurCmd="http://localhost:4200/commandes-livreur";
     //**********Historique pour Livreur******************
     public static String UrlLivreurHistorique="http://localhost:4200/historique-livreur";
+    //**********Client par Admin**********************
+    public static String UrlAdminClient="http://localhost:4200/clients";
     //**********Home page********************************
     public static String UrlHomePage="http://localhost:4200/";
     //**********Client Process*************************
     public static String UrlClientCommande="http://localhost:4200/commandes-client";
     public static String UrlClientHistorique="http://localhost:4200/historique-client";
-    //*****************
+    public static String UrlLivreurCart="http://localhost:4200/map";
+    //************Edit Profil****************************
+    public static String UrlEditProfilePage="http://localhost:4200/profile";
+
 }
