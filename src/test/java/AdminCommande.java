6cfd2a9fd4e825590b1bdb74300538d824fd4de1
Lamrini Imane <125766708+ImaneLamrini@users.noreply.github.com>
2024-11-03 12:39:32 +0100

    Update AdminCommande.java

--- src/test/java/AdminCommande.java
+++ src/test/java/AdminCommande.java
@@ -1,4 +1,6 @@
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
@@ -23,7 +25,83 @@ public class AdminCommande extends Loginadmin{
 
     }
 
-@Test(priority=2)
+    @Test(priority=2)
+    public void AssignerLivreur() throws InterruptedException {
+        boolean livreurAssignationTerminee = false;
+
+        while (!livreurAssignationTerminee) {
+            try {
+                // Attendre la visibilité du bouton "choisir livreur"
+                WebElement choisirLivreur = wait.until(ExpectedConditions.visibilityOfElementLocated(Project_Xpath.xpathchoisirLivreur));
+
+                // Si le bouton "choisir livreur" est visible, exécuter les étapes d'assignation
+                if (choisirLivreur.isDisplayed()) {
+                    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", choisirLivreur);
+
+                    // Vérifier l'affichage du PopUp d'assignation
+                    softAssert.assertTrue(ProjectFunctions.VerifyElements(driver, Project_Xpath.xpathPopUpAssigner));
+                    if (ProjectFunctions.VerifyElements(driver, Project_Xpath.xpathPopUpAssigner)) {
+                        System.out.println("Test Passed: Un PopUp de choix de livreur est affiché correctement");
+                    }
+
+                    // Sélectionner un livreur
+                    WebElement selectLivreur = driver.findElement(Project_Xpath.xpathSelectLivreur);
+                    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", selectLivreur);
+                    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", selectLivreur);
+                    //WebElement selectLivreur = wait.until(ExpectedConditions.visibilityOfElementLocated(Project_Xpath.xpathSelectLivreur));
+                    //selectLivreur.click();
+                    System.out.println("Le livreur est sélectionné!");
+
+                    // Cliquer sur le bouton d'assignation
+                    WebElement assignerLivreur = driver.findElement(Project_Xpath.xpathassignerLivreur);
+                    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", assignerLivreur);
+                    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", assignerLivreur);
+                    //WebElement assignerLivreur = wait.until(ExpectedConditions.visibilityOfElementLocated(Project_Xpath.xpathassignerLivreur));
+                    //assignerLivreur.click();
+                    softAssert.assertTrue(ProjectFunctions.VerifyElements(driver, Project_Xpath.xpathSuccessAssigner),
+                            "Problème lors de l'assignation de livreur à cette commande");
+                    if (ProjectFunctions.VerifyElements(driver, Project_Xpath.xpathSuccessAssigner)) {
+                        System.out.println("Test Passed: Un Livreur disponible est bien assigné à la commande!");
+                    }
+
+                    // Confirmer l'assignation en cliquant sur "OK"
+                    Thread.sleep(1000);
+                    WebElement OK = wait.until(ExpectedConditions.visibilityOfElementLocated(Project_Xpath.xpath_OK_Assignation));
+                    OK.click();
+
+                    // Fin de l'assignation pour cette commande
+                    livreurAssignationTerminee = true;
+                }
+
+            } catch (TimeoutException e) {
+                // Si le bouton "choisir livreur" n'est pas trouvé dans le délai, passer à la page suivante
+                try {
+                    WebElement nextPageButton = driver.findElement(Project_Xpath.xpathNextPage);
+
+                    // Vérifier si le bouton "Next" est activé avant de cliquer
+                    if (nextPageButton.isEnabled()) {
+                        nextPageButton.click();
+                        Thread.sleep(1000); // Attendre le chargement de la page
+                    } else {
+                        System.out.println("No more pages, 'choisir livreur' button not found.");
+                        break;
+                    }
+                } catch (NoSuchElementException ex) {
+                    System.out.println("Le bouton 'Next' n'est pas disponible.");
+                    break;
+                }
+            }
+        }
+
+        // Assertion pour consolider les résultats
+        softAssert.assertAll();
+    }
+
+
+
+
+
+    /*@Test(priority=2)
     public void AssignerLivreur() throws InterruptedException {
         WebElement choisirLivreur = wait.until(ExpectedConditions.visibilityOfElementLocated(Project_Xpath.xpathchoisirLivreur));
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();", choisirLivreur);
@@ -51,7 +129,7 @@ public class AdminCommande extends Loginadmin{
         //WebElement OK=driver.findElement(Project_Xpath.xpath_OK_Assignation);
         OK.click();
        // softAssert.assertAll();
-}
+}*/
 @Test(priority=4)
     public void ConsulterLivreur(){
         //Click on Livreur button in the side-bar
@@ -62,14 +140,7 @@ public class AdminCommande extends Loginadmin{
             System.out.println("Test Passed: La liste des livreurs s'affiche correctement!");
         }
 }
-/*@Test(priority=3)
-    public void ChangeStatus(){
-    // Verifying status change to "En cours"
-    WebElement statusElement = wait.until(ExpectedConditions.visibilityOfElementLocated(Project_Xpath.xpathStatusElement));
-    String updatedStatus = statusElement.getText();
-    Assert.assertEquals(updatedStatus, "En cours", "Le statut de la commande n'a pas changé correctement.");
-    System.out.println("Test Passed: Le statut de la commande a changé à 'En cours' après l'assignation du livreur.");
-    }*/
+
 @Test(priority=3)
 public void ConsulterHistoriqueAdmin(){
     //Click on Livreur button in the side-bar
@@ -81,10 +152,15 @@ public void ConsulterHistoriqueAdmin(){
         System.out.println("Test Passed: L'historique est bien consulté par l'administrateur!");
     }
 }
-//@Test(priority=5)
+@Test(priority=5)
     public void ConsulterClientParAdmin(){
-    //Main Code
-
+    WebElement Client=driver.findElement(Project_Xpath.xpathClientAdmin);
+    Client.click();
+    softAssert.assertTrue(ProjectFunctions.VerifyUrl(driver,Parameters.UrlAdminClient),"Un probléme lors de consulatation des clients par Admin!");
+    softAssert.assertTrue(ProjectFunctions.VerifyElements(driver, Project_Xpath.xpathListeClient),"Un probléme existe lors de consultation de liste des clients par l'admin!");
+    if(ProjectFunctions.VerifyUrl(driver,Parameters.UrlAdminClient)&&ProjectFunctions.VerifyElements(driver, Project_Xpath.xpathListeClient)){
+        System.out.println("Test Passed: L'historique est bien consulté par l'administrateur!");
+    }
 }
 //@Test(priority=6)
     public void ProfileAdmin(){
@@ -92,9 +168,12 @@ public void ConsulterHistoriqueAdmin(){
 }
 //****************Se déconnecter de l'aspace admin*****************************
 @Test(priority=7)
-public void SeDeconnecter(){
+public void SeDeconnecter() throws InterruptedException {
+    driver.findElement(Project_Xpath.dashAdminXpath).click();
+    Thread.sleep(1000);
     WebElement Logout=driver.findElement(Project_Xpath.logoutAdmin);
     Logout.click();
+    Thread.sleep(1000);
     Assert.assertTrue(ProjectFunctions.VerifyUrl(driver,Parameters.UrlHomePage),"L'admin n'a pas pu se déconnecter");
     if(ProjectFunctions.VerifyUrl(driver,Parameters.UrlHomePage)){
         System.out.println("Test Passed: l'admin se déconnecte correctement");
