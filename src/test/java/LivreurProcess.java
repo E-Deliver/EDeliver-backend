99d248019b0d2927762cea62c9fce913b957e385
Lamrini Imane <125766708+ImaneLamrini@users.noreply.github.com>
2024-11-04 13:12:59 +0100

    Update LivreurProcess.java

--- src/test/java/LivreurProcess.java
+++ src/test/java/LivreurProcess.java
@@ -79,112 +79,112 @@ public class LivreurProcess extends Loginlivreur{
 
     @Test(priority=2)
     public void livreurHistorique() throws InterruptedException {
-    WebElement Historique= driver.findElement(Project_Xpath.xpathHistoriqueLivreur);
-    Historique.click();
-    softAssert.assertTrue(ProjectFunctions.VerifyUrl(driver,Parameters.UrlLivreurHistorique),"Un probléme persiste lors de consultation d'historique de livreur!");
-    if (ProjectFunctions.VerifyUrl(driver,Parameters.UrlLivreurHistorique)){
-        System.out.println("Test Passed: l'historique des commandes de ce livreur est affiché correctement!");
+        WebElement Historique= driver.findElement(Project_Xpath.xpathHistoriqueLivreur);
+        Historique.click();
+        softAssert.assertTrue(ProjectFunctions.VerifyUrl(driver,Parameters.UrlLivreurHistorique),"Un probléme persiste lors de consultation d'historique de livreur!");
+        if (ProjectFunctions.VerifyUrl(driver,Parameters.UrlLivreurHistorique)){
+            System.out.println("Test Passed: l'historique des commandes de ce livreur est affiché correctement!");
+        }
+        softAssert.assertTrue(ProjectFunctions.VerifyElements(driver,Project_Xpath.xpathListeHistoriqueLivreur),"Un probléme persiste lors d'affichage des commandes assignés à ce livreur!");
+        if(ProjectFunctions.VerifyElements(driver,Project_Xpath.xpathListeHistoriqueLivreur)){
+            System.out.println("Test Passed: La liste des commandes livrés est affichée correctement comme historique");
+        }
+        Thread.sleep(1000);
     }
-    softAssert.assertTrue(ProjectFunctions.VerifyElements(driver,Project_Xpath.xpathListeHistoriqueLivreur),"Un probléme persiste lors d'affichage des commandes assignés à ce livreur!");
-    if(ProjectFunctions.VerifyElements(driver,Project_Xpath.xpathListeHistoriqueLivreur)){
-        System.out.println("Test Passed: La liste des commandes livrés est affichée correctement comme historique");
-    }
-    Thread.sleep(1000);
-}
 
-@Test(priority=3)
-public void CarteConsultation(){
+    @Test(priority=3)
+    public void CarteConsultation(){
         driver.findElement(Project_Xpath.dashxpathLivreur).click();
-    WebElement Cart= driver.findElement(Project_Xpath.xpathCartLivreur);
-    Cart.click();
-    softAssert.assertTrue(ProjectFunctions.VerifyUrl(driver,Parameters.UrlLivreurCart));
-    if(ProjectFunctions.VerifyUrl(driver,Parameters.UrlLivreurCart)){
-        System.out.println("Test Passed: la carte de localisation des commandes de ce livreur est correctement consultable");
+        WebElement Cart= driver.findElement(Project_Xpath.xpathCartLivreur);
+        Cart.click();
+        softAssert.assertTrue(ProjectFunctions.VerifyUrl(driver,Parameters.UrlLivreurCart));
+        if(ProjectFunctions.VerifyUrl(driver,Parameters.UrlLivreurCart)){
+            System.out.println("Test Passed: la carte de localisation des commandes de ce livreur est correctement consultable");
+        }
     }
-}
 
-@Test(priority=4)
+    @Test(priority=4)
     public void EditProfileLivreur() throws InterruptedException {
 
-    //WebElement EditProfile = wait.until(ExpectedConditions.visibilityOfElementLocated(Project_Xpath.xpathEditProfileLivreur));
-    //((JavascriptExecutor) driver).executeScript("arguments[0].click();", EditProfile);
-    //driver.findElement(By.xpath("//*[@id=\"mySidebar\"]/div[2]/a[5]")).click();
-    WebElement EditProfile= driver.findElement(Project_Xpath.xpathEditProfileLivreur);
-    EditProfile.click();
-    //Page is well displayed
-    softAssert.assertTrue(ProjectFunctions.VerifyUrl(driver,Parameters.UrlEditProfilePage),"La page d'edit profile pour livreur ne s'affiche pas correctement!");
-    if(ProjectFunctions.VerifyUrl(driver,Parameters.UrlEditProfilePage)){
-        System.out.println("Test passed: La page d'edit profile pour le livreur est affichée correctement");
-    }
-    //Coté notification
-    WebElement notifications = wait.until(ExpectedConditions.visibilityOfElementLocated(Project_Xpath.AppearingOfNotification));
+        //WebElement EditProfile = wait.until(ExpectedConditions.visibilityOfElementLocated(Project_Xpath.xpathEditProfileLivreur));
+        //((JavascriptExecutor) driver).executeScript("arguments[0].click();", EditProfile);
+        //driver.findElement(By.xpath("//*[@id=\"mySidebar\"]/div[2]/a[5]")).click();
+        WebElement EditProfile= driver.findElement(Project_Xpath.xpathEditProfileLivreur);
+        EditProfile.click();
+        //Page is well displayed
+        softAssert.assertTrue(ProjectFunctions.VerifyUrl(driver,Parameters.UrlEditProfilePage),"La page d'edit profile pour livreur ne s'affiche pas correctement!");
+        if(ProjectFunctions.VerifyUrl(driver,Parameters.UrlEditProfilePage)){
+            System.out.println("Test passed: La page d'edit profile pour le livreur est affichée correctement");
+        }
+        //Coté notification
+        WebElement notifications = wait.until(ExpectedConditions.visibilityOfElementLocated(Project_Xpath.AppearingOfNotification));
 
-    //WebElement notifications= driver.findElement(Project_Xpath.AppearingOfNotification);
-    softAssert.assertTrue(notifications.isDisplayed(),"la liste des notifications n'apparait pas correctemet!");
-    if(notifications.isDisplayed()){
-        System.out.println("Test Passed: La liste des notifications s'affiche correctement");
-    }
-    Thread.sleep(1000);
-    //Coté edit
-    //click on edit icon
-    WebElement edit= driver.findElement(Project_Xpath.XpathEditProfile);
-    edit.click();
-    Thread.sleep(1000);
-    //Edit password
-    WebElement passwordinput =driver.findElement(Project_Xpath.xpathPasswordInput);
-    passwordinput.clear();
-    passwordinput.sendKeys("$2a$10$w3PL6XU0VqJjYjrR5RUxhuDcZ3G0aYQwcGlPf5eKz4pBrKHd1VSS2");
-    System.out.println("The new Password was entered!");
-    //Edit adress
-    WebElement adressUpdate= driver.findElement(Project_Xpath.xpathAdressinput);
-    String textadress= adressUpdate.getText();
-    if(textadress.equals("Rabat")){
-        adressUpdate.clear();
-        adressUpdate.sendKeys("Fes");
-        System.out.println("The new Adress was entered!");
-    }
-    else{
-        adressUpdate.clear();
-        adressUpdate.sendKeys("Rabat");
-        System.out.println("The new Adress was entered!");
+        //WebElement notifications= driver.findElement(Project_Xpath.AppearingOfNotification);
+        softAssert.assertTrue(notifications.isDisplayed(),"la liste des notifications n'apparait pas correctemet!");
+        if(notifications.isDisplayed()){
+            System.out.println("Test Passed: La liste des notifications s'affiche correctement");
+        }
+        Thread.sleep(1000);
+        //Coté edit
+        //click on edit icon
+        WebElement edit= driver.findElement(Project_Xpath.XpathEditProfile);
+        edit.click();
+        Thread.sleep(1000);
+        //Edit password
+        WebElement passwordinput =driver.findElement(Project_Xpath.xpathPasswordInput);
+        passwordinput.clear();
+        passwordinput.sendKeys("$2a$10$w3PL6XU0VqJjYjrR5RUxhuDcZ3G0aYQwcGlPf5eKz4pBrKHd1VSS2");
+        System.out.println("The new Password was entered!");
+        //Edit adress
+        WebElement adressUpdate= driver.findElement(Project_Xpath.xpathAdressinput);
+        String textadress= adressUpdate.getText();
+        if(textadress.equals("Rabat")){
+            adressUpdate.clear();
+            adressUpdate.sendKeys("Fes");
+            System.out.println("The new Adress was entered!");
+        }
+        else{
+            adressUpdate.clear();
+            adressUpdate.sendKeys("Rabat");
+            System.out.println("The new Adress was entered!");
+            Thread.sleep(500);
+        }
+        //Save updates
+        WebElement SAVE = driver.findElement(Project_Xpath.XpathSaveUpdateProfile);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", SAVE);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", SAVE);
+
+        try {
+            Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+            System.out.println("Alert text: " + alert.getText());
+            alert.accept();// Accepter l'alerte
+            System.out.println("Alert accepted.");
+        } catch (NoAlertPresentException e) {
+            System.out.println("No alert found after clicking save.");
+        }
+
+        // Check if update was successful
+        softAssert.assertTrue(ProjectFunctions.VerifyElements(driver, Project_Xpath.XpathSuccessUpdateProfile), "La modification n'est pas faite correctement!");
+        if (ProjectFunctions.VerifyElements(driver, Project_Xpath.XpathSuccessUpdateProfile)) {
+            System.out.println("Test Passed: la modification est bien faite de l'adresse et donc les modifications travaille correctement!");
+        }
+
+        // Click on OK button
+        //WebElement OK_Modif = driver.findElement(Project_Xpath.xpathOKUpdateProfil);
+        //OK_Modif.click();
         Thread.sleep(500);
     }
-    //Save updates
-    WebElement SAVE = driver.findElement(Project_Xpath.XpathSaveUpdateProfile);
-    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", SAVE);
-    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", SAVE);
 
-    try {
-        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
-        System.out.println("Alert text: " + alert.getText());
-        alert.accept();  // Accepter l'alerte
-        System.out.println("Alert accepted.");
-    } catch (NoAlertPresentException e) {
-        System.out.println("No alert found after clicking save.");
-    }
-
-    // Check if update was successful
-    softAssert.assertTrue(ProjectFunctions.VerifyElements(driver, Project_Xpath.XpathSuccessUpdateProfile), "La modification n'est pas faite correctement!");
-    if (ProjectFunctions.VerifyElements(driver, Project_Xpath.XpathSuccessUpdateProfile)) {
-        System.out.println("Test Passed: la modification est bien faite de l'adresse et donc les modifications travaille correctement!");
-    }
-
-    // Click on OK button
-    //WebElement OK_Modif = driver.findElement(Project_Xpath.xpathOKUpdateProfil);
-    //OK_Modif.click();
-    Thread.sleep(500);
-}
-
-@Test(priority=5)
+    @Test(priority=5)
     public void SeDeconnecter(){
-    driver.findElement(Project_Xpath.XpathdashlivreurButton).click(); //Click on dashboard button
-    WebElement Logout=driver.findElement(Project_Xpath.logoutlivreur);
-    Logout.click();
-    softAssert.assertTrue(ProjectFunctions.VerifyUrl(driver,Parameters.UrlHomePage),"Ce livreur n'a pas pu se déconnecter");
-    if(ProjectFunctions.VerifyUrl(driver,Parameters.UrlHomePage)){
-        System.out.println("Test Passed: le livreur se déconnecte correctement");
+        driver.findElement(Project_Xpath.XpathdashlivreurButton).click(); //Click on dashboard button
+        WebElement Logout=driver.findElement(Project_Xpath.logoutlivreur);
+        Logout.click();
+        softAssert.assertTrue(ProjectFunctions.VerifyUrl(driver,Parameters.UrlHomePage),"Ce livreur n'a pas pu se déconnecter");
+        if(ProjectFunctions.VerifyUrl(driver,Parameters.UrlHomePage)){
+            System.out.println("Test Passed: le livreur se déconnecte correctement");
+        }
     }
-}
 
 
 }
