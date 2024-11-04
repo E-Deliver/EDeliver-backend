47f9c08a552a1fb684d4bc8e92c97679ee634678
Lamrini Imane <125766708+ImaneLamrini@users.noreply.github.com>
2024-11-04 13:12:28 +0100

    Update ClientProcess.java

--- src/test/java/ClientProcess.java
+++ src/test/java/ClientProcess.java
@@ -1,4 +1,4 @@
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -36,9 +36,59 @@ public class ClientProcess extends Loginclient{
         }
         Thread.sleep(1000);
     }
+    @Test(priority=3)
+    public void editProfile() throws InterruptedException {
+        WebElement profile=driver.findElement(Project_Xpath.xpathEditProfileClient);
+        profile.click();
+        //Page is well displayed
+        softAssert.assertTrue(ProjectFunctions.VerifyUrl(driver,Parameters.UrlEditProfileClientPage),"La page d'edit profile pour client ne s'affiche pas correctement!");
+        if(ProjectFunctions.VerifyUrl(driver,Parameters.UrlEditProfileClientPage)){
+            System.out.println("Test passed: La page d'edit profile pour le client est affichée correctement");
+        }
+        //Coté notification
+        WebElement notifications = wait.until(ExpectedConditions.visibilityOfElementLocated(Project_Xpath.AppearingOfNotification));
 
-    @Test(priority = 4)
+        //WebElement notifications= driver.findElement(Project_Xpath.AppearingOfNotification);
+        softAssert.assertTrue(notifications.isDisplayed(),"la liste des notifications n'apparait pas correctemet!");
+        if(notifications.isDisplayed()){
+            System.out.println("Test Passed: La liste des notifications s'affiche correctement");
+        }
+        Thread.sleep(1000);
+        //Coté edit
+
+        WebElement edit= driver.findElement(Project_Xpath.XpathEditProfile);
+        edit.click();
+        Thread.sleep(1000);
+        //Edit password
+        WebElement passwordinput =driver.findElement(Project_Xpath.xpathPasswordInput);
+        passwordinput.clear();
+        passwordinput.sendKeys("$2a$10$w3PL6XU0VqJjYjrR5RUxhuDcZ3G0aYQwcGlPf5eKz4pBrKHd1VSS2");
+        System.out.println("The new Password was entered!");
+        //Edit client's name
+        WebElement nameUpdate= driver.findElement(Project_Xpath.xpathNameinput);
+        nameUpdate.clear();
+        nameUpdate.sendKeys("ClientTest");
+        System.out.println("The new client's name was entered!");
+
+        //Save updates
+        WebElement SAVE = driver.findElement(Project_Xpath.XpathSaveUpdateProfile);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", SAVE);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", SAVE);
+
+        // Check if update was successful
+        softAssert.assertTrue(ProjectFunctions.VerifyElements(driver, Project_Xpath.XpathSuccessUpdateProfile), "La modification n'est pas faite correctement!");
+        if (ProjectFunctions.VerifyElements(driver, Project_Xpath.XpathSuccessUpdateProfile)) {
+            System.out.println("Test Passed: la modification est bien faite de l'adresse et donc les modifications travaille correctement!");
+        }
+        // Click on OK button
+        WebElement OK_Modif = driver.findElement(Project_Xpath.xpathOKUpdateProfil);
+        OK_Modif.click();
+        Thread.sleep(500);
+    }
+    //@Test(priority = 4)
     public void SeDeconnecter(){
+        //Cick on dashboard before logout
+        driver.findElement(By.xpath("//*[@id=\"mySidebar\"]/div[2]/a[1]")).click();
         WebElement Logout=driver.findElement(Project_Xpath.logoutClient);
         Logout.click();
         Assert.assertTrue(ProjectFunctions.VerifyUrl(driver,Parameters.UrlHomePage),"Le client n'a pas pu se déconnecter");
